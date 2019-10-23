fe8ea05264a9f74c8b1909d4a7dd3eac5f476f88
Bao Doan <doanthaibao@gmail.com>
2019-10-23 23:12:08 +0700

    Adding auto-create schema for application

--- src/main/java/bao/doan/learning/LearningApplication.java
+++ src/main/java/bao/doan/learning/LearningApplication.java
@@ -2,8 +2,14 @@ package bao.doan.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import javax.persistence.Entity;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"bao.doan.learning.model"})
 public class LearningApplication {
 
 	public static void main(String[] args) {
